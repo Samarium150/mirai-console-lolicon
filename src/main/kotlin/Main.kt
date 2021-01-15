1253d35a92518f5cf2a39e198349f57715114ed4
Samarium <28302241+Samarium150@users.noreply.github.com>
2021-01-15 08:38:55 -0500

    build: update mirai-console version to 2.0.0

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -44,7 +44,7 @@ import net.mamoe.mirai.utils.info
 object Main: KotlinPlugin(
     JvmPluginDescription(
         id = "com.github.samarium150.mirai-console-lolicon",
-        version = "1.5.1",
+        version = "1.5.2",
         name = "mirai-console-lolicon"
     )
 ) {
