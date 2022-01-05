2f052be004b77e3e4c5fde22a820019de087d41e
Samarium <28302241+Samarium150@users.noreply.github.com>
2022-01-05 20:16:11 +0800

    build: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.net.URL
 
 plugins {
-    val kotlinVersion = "1.5.10"
+    val kotlinVersion = "1.5.30"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.8.3"
+    id("net.mamoe.mirai-console") version "2.9.2"
     id("org.jetbrains.dokka") version "1.6.0"
     id("com.geoffgranum.gradle-conventional-changelog") version "0.3.1"
 }
