2ed4b3e7c0a4c5bc749c2c5f23e3aa086fa5bc01
Samarium <28302241+Samarium150@users.noreply.github.com>
2022-10-13 12:35:32 -0600

    fix: correct verbose setting in forward mode

--- src/main/kotlin/MiraiConsoleLolicon.kt
+++ src/main/kotlin/MiraiConsoleLolicon.kt
@@ -35,7 +35,7 @@ import java.net.Proxy
 object MiraiConsoleLolicon : KotlinPlugin(
     JvmPluginDescription(
         id = "io.github.samarium150.mirai.plugin.mirai-console-lolicon",
-        version = "6.0.4",
+        version = "6.0.5",
         name = "Lolicon"
     ) {
         author("Samarium150")
