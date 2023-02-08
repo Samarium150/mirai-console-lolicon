aa36642c1927ce1903dad42a5533e34e1d9dec4a
Samarium150 <28302241+Samarium150@users.noreply.github.com>
2023-02-08 14:46:32 -0700

    build: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "1.7.10"
+    val kotlinVersion = "1.8.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.12.3"
+    id("net.mamoe.mirai-console") version "2.14.0"
     id("com.geoffgranum.gradle-conventional-changelog") version "+"
 }
 
@@ -17,18 +17,10 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-client-okhttp-jvm:2.1.3") {
-        exclude(group = "org.jetbrains.kotlin")
-        exclude(group = "org.jetbrains.kotlinx")
-    }
-    implementation("io.ktor:ktor-client-content-negotiation:2.1.3") {
-        exclude(group = "org.jetbrains.kotlin")
-        exclude(group = "org.jetbrains.kotlinx")
-    }
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.3") {
-        exclude(group = "org.jetbrains.kotlin")
-        exclude(group = "org.jetbrains.kotlinx")
-    }
+    implementation(platform("io.ktor:ktor-bom:2.2.3"))
+    implementation("io.ktor:ktor-client-okhttp-jvm")
+    implementation("io.ktor:ktor-client-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
 }
 
 tasks {
